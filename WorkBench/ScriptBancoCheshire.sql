fce2a80a346747e13ccfc34b7372fde92a59f7dd
henrique17h <45178432+henrique17h@users.noreply.github.com>
2019-06-17 14:29:09 -0300

    Atualização do script de banco de dados
    Nesse momento foram atualizadas as descrições dos campos da criação do banco de dados

--- WorkBench/ScriptBancoCheshire.sql
+++ WorkBench/ScriptBancoCheshire.sql
@@ -2,27 +2,30 @@ create database cheshire_php;
 use cheshire_php;
 
 create table login(
+idLogin int not null auto_increment,
 user varchar(255),
 senha varchar(255),
-id_user_fk
+constraint idLogin_fk primary key (idLogin)
 );
 
 create table aluno(
-id_aluno,
-nome,
-sobrenome,
-dt_nascimento,
+idAluno int not null auto_increment,
+nome varchar (100),
+sobrenome varchar (75),
+dt_nascimento datetime null default now(),
+constraint idAluno_fk primary key (idAluno)
 );
 
 create table orientador(
-id_Ori,
-nome,
-sobrenome,
-dt_nascimento,
+idOri int not null auto_increment,
+nome varchar (100),
+sobrenome varchar (75),
+dt_nascimento datetime null default now(),
+constraint idOri_fk primary key(idOri)
 );
 
 create table familiar(
-id_fam,
+id_Fm,
 nome,
 sobrenome,
 dt_nascimento,
@@ -31,19 +34,20 @@ parentesco,
 );
 
 create table curso(
-id_cur,
-nome_cur,
-periodo
+idCurso int not null auto_increment,
+nome varchar (100),
+periodo varchar (15),
+constraint idCurso_fk primary key (idCurso)
 );
 
 create table endereco(
-id_end,
-cep,
-rua,
-bairro,
-cidade,
-estado,
-complemento
+id_endereco int not null auto_increment,
+cep varchar (8),
+rua varchar (75),
+bairro varchar (75),
+cidade varchar (75),
+estado varchar (75),
+complemento varchar (50)
 );
 
 create table contato(
@@ -58,3 +62,4 @@ insert into usuario(user, senha) values ('teste','4967');
 select * from usuario;
 describe usuario;
 drop table usuario;
+--
