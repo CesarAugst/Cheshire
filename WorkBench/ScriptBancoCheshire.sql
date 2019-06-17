7f6e449507f9f682f5df4e0c19160856694d49fe
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2019-06-17 13:56:59 -0300

    Mudanças no banco
    Criadas novas tabelas
    
    Co-Authored-By: henrique17h <henrique17h@users.noreply.github.com>

--- WorkBench/ScriptBancoCheshire.sql
+++ WorkBench/ScriptBancoCheshire.sql
@@ -1,9 +1,57 @@
 create database cheshire_php;
 use cheshire_php;
 
-create table usuario(
+create table login(
 user varchar(255),
-senha varchar(255));
+senha varchar(255),
+id_user_fk
+);
+
+create table aluno(
+id_aluno,
+nome,
+sobrenome,
+dt_nascimento,
+);
+
+create table orientador(
+id_Ori,
+nome,
+sobrenome,
+dt_nascimento,
+);
+
+create table familiar(
+id_fam,
+nome,
+sobrenome,
+dt_nascimento,
+parentesco,
+
+);
+
+create table curso(
+id_cur,
+nome_cur,
+periodo
+);
+
+create table endereco(
+id_end,
+cep,
+rua,
+bairro,
+cidade,
+estado,
+complemento
+);
+
+create table contato(
+id_cont,
+celular,
+telefone,
+email
+);
 
 insert into usuario(user, senha) values ('teste','4967');
 
