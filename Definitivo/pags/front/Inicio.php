1491261376c1b6afa5b5926857c1ae1c76928451
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2019-07-22 21:24:18 -0300

    Footer
    De todas as páginas o footer foi retirado do corpo e colocado em um aquivo unico

--- Definitivo/pags/front/Inicio.php
+++ Definitivo/pags/front/Inicio.php
@@ -221,39 +221,3 @@ session_destroy();
 				</div>	
 			</section>
 			<!-- End post-content Area -->
-			
-			<!-- start footer Area -->		
-			<footer class="footer-area section-gap">
-				<div class="container">
-                                    <img src="img/logo_cps.png">
-					<div class="row footer-bottom d-flex justify-content-between">
-						<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> Etec de Cotia <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://www.cps.sp.gov.br" target="_blank">Centro Paula Souza</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-						<div class="col-lg-4 col-sm-12 footer-social">
-							<a href="https://www.facebook.com/etecdecotia"><i class="fa fa-facebook"></i></a>
-							<a href="https://twitter.com/etecdecotia"><i class="fa fa-twitter"></i></a>
-						</div>					
-					</div>
-				</div>
-			</footer>
-			<!-- End footer Area -->
-
-
-			<script src="js/vendor/jquery-2.2.4.min.js"></script>
-			<script src="js/popper.min.js"></script>
-			<script src="js/vendor/bootstrap.min.js"></script>			
-			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
- 			<script src="js/jquery-ui.js"></script>					
-  			<script src="js/easing.min.js"></script>			
-			<script src="js/hoverIntent.js"></script>
-			<script src="js/superfish.min.js"></script>	
-			<script src="js/jquery.ajaxchimp.min.js"></script>
-			<script src="js/jquery.magnific-popup.min.js"></script>	
-    		<script src="js/jquery.tabs.min.js"></script>						
-			<script src="js/jquery.nice-select.min.js"></script>	
-			<script src="js/owl.carousel.min.js"></script>									
-			<script src="js/mail-script.js"></script>	
-			<script src="js/main.js"></script>	
-		</body>
-        </html>
