0d302ff1af41dc7633dfd20323db139e490293f6
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2019-08-11 20:01:41 -0300

    criação de views
    Atalhos de visualização de querys no banco de daos cridas, entre elas uma visualização para alunos que são maiores e alunos que são menores de idade

--- CESAR/ScriptCesar.sql
+++ CESAR/ScriptCesar.sql
@@ -4,6 +4,12 @@
 create database bdCheshire; -- Cria o banco de dados para a aplicação
 use bdCheshire; -- Altera o banco de dados utilizado para o da aplicação
 
+
+insert into tblcadastro values(1,'teste','primeiro','20190811','1','1');
+select * from tblcadastro;
+select * from tbldtregistro;
+
+
 CREATE TABLE tblcadastro (
     id INT,
     nome VARCHAR(50),
@@ -53,14 +59,7 @@ CREATE TABLE tblendereco (
         REFERENCES id (tblcadastro)
 );
 
-CREATE TABLE tblendereco (
-    idusuario INT,
-    logradouro VARCHAR(50),
-    cidade VARCHAR(50),
-    bairro VARCHAR(50),
-    FOREIGN KEY (idusuario)
-        REFERENCES id (tblcadastro)
-);
+
 
 CREATE TABLE tbltexto (
     idconteudo INT,
@@ -83,3 +82,28 @@ CREATE TABLE tblturma (
 create table tbltipousuario(
 idtipo int,
 tipo varchar(50));
+
+create table tblmensagem(
+idmensagem int,
+remetente int,
+destinatario int,
+conteudo text,
+data_de_envio date,
+primary key (idmensagem),
+foreign key (remetente) references id(tblcadastro),
+foreign key (destinatario) references id(tblcadastro)
+);
+
+CREATE VIEW ver_tudo_cadastro AS SELECT * FROM tblcadastro;
+CREATE VIEW ver_tudo_login AS SELECT * FROM tbllogin;
+CREATE VIEW ver_tudo_contatos AS SELECT * FROM tblcontato;
+CREATE VIEW ver_tudo_endereco AS SELECT * FROM tblendereco;
+CREATE VIEW ver_apenas_o_rm AS SELECT id FROM tblcadastro;
+CREATE VIEW ver_as_mensagens_enviadas AS SELECT conteudo FROM tblmensagem;
+CREATE VIEW ver_periodo_noturno AS SELECT nome FROM tblturma where periodo = noturno;
+CREATE VIEW ver_periodo_diurno AS SELECT nome FROM tblturma where periodo = diurno;
+CREATE VIEW ver_alunos_menores AS SELECT id,nome,sobrenome FROM tblcadastro where (timestampdiff(year,dt_nascimento,now)<18);
+CREATE VIEW ver_alunos_terceiristas AS SELECT id,nome,sobrenome FROM tblcadastro where (timestampdiff(year,dt_nascimento,now())>18);
+
+select timestampdiff(year,'2018-08-11',now());
+select year(now());
