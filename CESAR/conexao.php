d09b0baae5cacf56d058f819a8c20583f5ceea82
CesarAugst <45099916+CesarAugst@users.noreply.github.com>
2019-09-04 13:46:55 -0300

    Apaguei tudo
    Começar o site do zero a partir de sua funcionalidade

--- CESAR/conexao.php
+++
@@ -1,5 +0,0 @@
-<?php
-// conecta o banco de dados MYSQL
-$conexao = mysqli_connect('localhost', 'root','','projeto_php','3307'); // Deu erro porque tava escrito "msql_connect" (falta de atençao)
-
-?>
