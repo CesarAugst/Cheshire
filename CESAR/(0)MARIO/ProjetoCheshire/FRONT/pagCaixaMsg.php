b4b471676958cfc95e1b99e925d33c23f40298d5
Gabriel <mrjumper360@gmail.com>
2019-09-30 23:34:24 -0300

    adicionado o front-end beta da página

--- CESAR/(0)MARIO/ProjetoCheshire/FRONT/pagCaixaMsg.php
+++ CESAR/(0)MARIO/ProjetoCheshire/FRONT/pagCaixaMsg.php
@@ -7,59 +7,73 @@ session_start();
 ?>
 
 <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <meta charset="UTF-8">
     <title></title>
 </head>
 
 <body>
-    <a href="pagEnviar" title="Clique para enviar uma mensagem">Enviar mensagem</a><br>
-    <a href="pagMenuPrincipal" title="Retornar ao menu principal">menu</a><br>
-    <a href="pagMsgLida" title="Visualizar mensagens lidas">Mensagens lidas</a><br>
-    <h1>Caixa de entrada de <?php echo $_SESSION['nome'] ?></h1><br>
+ <nav class="navbar navbar-light bg-dark text-light">
+ <table>
+<tr>
+    <td><a class = "btn btn-success" href="pagEnviar" title="Clique para enviar uma mensagem">Enviar mensagem</a></td>
+    <td><a class = "btn btn-success" href="pagMenuPrincipal" title="Retornar ao menu principal">menu</a></td>
+    <td><a class = "btn btn-success" href="pagMsgLida" title="Visualizar mensagens lidas">Mensagens lidas</a><br></td>
+</tr>
+</table>
+</nav>
+<br><br>
+    <div class = "container text-center">
+        <h1>Caixa de entrada de <?php echo $_SESSION['nome'] ?></h1><br>
 
 
-    <table border="1">
-        <tr>
-            <td>id da Mensagem</td>
-            <td>Remetente</td>
-            <td>Destinatario</td>
-            <td>Conteudo</td>
-            <td>Anonimato</td>
-            <td>Enviada</td>
-            <td>Lida</td>
-            <td>Quando foi lida</td>
-            <td>Exlcuir</td>
-            <td>Responder</td>
-
-        </tr>
-
-        <?php
-
-        $mensagens = listaMensagens($conexao, $_SESSION['nome']);
-
-        foreach ($mensagens as $mensagem) :
-            ?>
-
+        <table class = "table table-borderless table-hover" border="1">
             <tr>
-                <td><?php echo $mensagem['cod_mensagem'] ?></td>
-                <td><?php echo $mensagem['remetente'] ?></td>
-                <td><?php echo $mensagem['destinatario'] ?></td>
-                <td><?php echo $mensagem['conteudo'] ?></td>
-                <td><?php echo $mensagem['anonimato'] ?></td>
-                <td><?php echo $mensagem['dataEnviada'] ?></td>
-                <td><?php echo $mensagem['statusLida'] ?></td>
-                <td><?php echo $mensagem['dataLida'] ?></td>
-                <td><a href="../BACK/lida.php?cod_mensagem=
-                       <?php echo $mensagem['cod_mensagem'] ?>">Marcar como lida</a>
-                </td>
-                <td><a href="pagResponderMsg.php?cod_mensagem=
-                       <?php echo $mensagem['cod_mensagem'] ?>">Responder</a></td>
-            </tr>
-        <?php
-        endforeach;
-        ?>
-    </table>
+                <td>id da Mensagem</td>
+                <td>Remetente</td>
+                <td>Destinatario</td>
+                <td>Conteudo</td>
+                <td>Anonimato</td>
+                <td>Enviada</td>
+                <td>Lida</td>
+                <td>Quando foi lida</td>
+                <td>Exlcuir</td>
+                <td>Responder</td>
 
+            </tr>
+
+            <?php
+
+            $mensagens = listaMensagens($conexao, $_SESSION['nome']);
+
+            foreach ($mensagens as $mensagem) :
+                ?>
+
+                <tr>
+                    <td><?php echo $mensagem['cod_mensagem'] ?></td>
+                    <td><?php echo $mensagem['remetente'] ?></td>
+                    <td><?php echo $mensagem['destinatario'] ?></td>
+                    <td><?php echo $mensagem['conteudo'] ?></td>
+                    <td><?php echo $mensagem['anonimato'] ?></td>
+                    <td><?php echo $mensagem['dataEnviada'] ?></td>
+                    <td><?php echo $mensagem['statusLida'] ?></td>
+                    <td><?php echo $mensagem['dataLida'] ?></td>
+                    <td><a href="../BACK/lida.php?cod_mensagem=
+                        <?php echo $mensagem['cod_mensagem'] ?>">Marcar como lida</a>
+                    </td>
+                    <td><a href="pagResponderMsg.php?cod_mensagem=
+                        <?php echo $mensagem['cod_mensagem'] ?>">Responder</a></td>
+                </tr>
+            <?php
+            endforeach;
+            ?>
+        </table>
+    </div>
+<footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</footer>
 </body>
 
 </html>
