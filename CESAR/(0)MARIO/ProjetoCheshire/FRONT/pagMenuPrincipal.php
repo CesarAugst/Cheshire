46b75c7d2c04cb38b70e59e51651376d6d1a5f27
Gabriel <mrjumper360@gmail.com>
2019-10-01 01:04:59 -0300

    modificações no front-end da página

--- CESAR/(0)MARIO/ProjetoCheshire/FRONT/pagMenuPrincipal.php
+++ CESAR/(0)MARIO/ProjetoCheshire/FRONT/pagMenuPrincipal.php
@@ -1,33 +1,47 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <?php
 session_start();
 if ($_SESSION['log'] != "ativo"){
     session_destroy();
     header("location: pag_login.php");
 }
-echo "Olá, <b>" .$_SESSION['nome']. 
-        "</b>, bem vindo ao sistema. ";
-echo "<a href='../BACK/logout.php'>SAIR</a>"
+echo "<nav class = 'navbar navbar-light bg-dark text-light'>
+    <a class = 'navbar-brand'> Olá, <em><b>" .$_SESSION['nome']. 
+        "</em></b>, bem vindo ao sistema.</a>";
+echo "<a class = 'btn btn-success btn-lg' href='../BACK/logout.php'>SAIR</a>
+      </nav>
+    ";
 ?>
 <html>
     <head>
+    
         <meta charset="UTF-8">
         <title></title>
     </head>
+    <div style = "background-color: green">
     <body>
-        <h1>Bem vindo ao sistema</h1>
-    <li><a href="pagEnviar.php">Enviar mensagem</a>
-    
-            <?php
-            $_SESSION['verifica'] = 'ativo';
-            ?>     
-        </ul></li>
-    <li><a href="#">Gerenciamento</a>
-        <ul><li><a href="pag_listar_cliente.php">Cliente</a></li>
-            <?php
-            $_SESSION['verifica'] = 'ativo';
-            die();
-            ?>
-        </ul></li>
+    <div class = "container text-center text-light" style="background-color: black">
+        <div class = "col-md-12">
+                <br><br><h1>Bem vindo ao sistema</h1><br><br>
+            <a class = "btn btn-block btn-lg btn-success" href="pagEnviar.php">Enviar mensagem
+                    <?php
+                    $_SESSION['verifica'] = 'ativo';
+                    ?>     
+            </a><br>
+                
+            <a class = "btn btn-block btn-lg btn-success" href="#">
+                Gerenciamento
+            </a><br>
+
+                <a class = "btn btn-block btn-lg btn-success" href="pag_listar_cliente.php">Cliente
+                    <?php
+                    $_SESSION['verifica'] = 'ativo';
+                    die();
+                    ?>
+                </a><br>
+        </div>
+    </div>
+</div>
     </body>
 </html>
